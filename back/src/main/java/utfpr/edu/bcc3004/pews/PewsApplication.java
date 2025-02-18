dbc412339c7fdd840ea04fe8c2c3757c940d8a4a
atenokida <alexandretolomeotti@alunos.utfpr.edu.br>
2025-02-18 09:26:39 -0300

    Initial back-end setup

---
+++ back/src/main/java/utfpr/edu/bcc3004/pews/PewsApplication.java
@@ -0,0 +1,13 @@
+package utfpr.edu.bcc3004.pews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PewsApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/utfpr/edu/bcc3004/pews/PewsApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.edu.bcc3004.pews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
